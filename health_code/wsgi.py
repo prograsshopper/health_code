46d069575abd10fb1190a721c545974d4f513ee4
baidoosik <qoentlr37@naver.com>
2019-06-02 01:21:27 +0900

    fix wsgi

--- health_code/wsgi.py
+++ health_code/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_code.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'health_code.settings.prod_aws')
 
 application = get_wsgi_application()
